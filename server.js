e47d5c8d250bfcb678b4b2fd05736bc125140763
shahdipesh <dipeshasd@Dipeshs-MacBook-Pro.local>
2020-05-06 18:04:27 -0500

    handling createUser api

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ var express = require ('express');
 
 var pool = require('./connection');
 
-console.log(pool);
-
 var app = express();
+app.use(express.json());
 
+app.post('/createUser',(req,res)=>{
+var username = req.body.username;
+var password = req.body.password;
+pool.query(
+    `INSERT INTO messenger (username,password) VALUES (?,?);`,[username,password],(err,rows)=>{
+        if (err) throw err;
+        res.send (rows);
+    }
+)
+
+})
 
 
 app.listen(3000,()=>{
